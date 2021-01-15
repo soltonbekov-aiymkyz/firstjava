99898669f270bdaf7f85a54b83a31dda40be226e
Your Name <you@example.com>
2021-01-15 00:54:54 -0800

    kjkkfkb

--- first.js
+++ first.js
@@ -7,17 +7,61 @@ let fifth = fourth;
 let city = "city";
 let country = "country";
 
-let unit = "unit"
-let subjeckt = "subjeckt"
+let author = "oleg shestinskii"
+let  title = "blokada"
 
+ unit = "before"
+ subjeckt = "today "
+unit = "temarature"
+subjeckt = "pinaple"
+
+author = "kendji malle"
+title = "le gitange"
 
    console.log(fifth)
 
-  console.log(city +   " is the capital of great britain" +country)
+  console.log(city + " is the capital of great britain" +country)
   
-  console.log( unit +   "is measure in " + subjeckt )
+  console.log( unit + "is measure in " + subjeckt )
+   
+ console.log( "my favorite book is " + author +"by" + title + "!" )
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /*
+pos let - let in first  */
 
 
 
