4483892b6bf939db7281c0d3915cb02ecd5addd7
Your Name <you@example.com>
2021-01-15 00:26:24 -0800

    capital

--- first.js
+++ first.js
@@ -1,11 +1,20 @@
-let first = "aiymkyz";
+let first = "shrek";
 let secound = first;
 let third = secound ;
 let fourth = third;
 let fifth = fourth;
+let capital = "capital";
+let country = "country";
 
 
    console.log(fifth)
+  console.log(capital +" is the capital of great britain" +country)
+  
+  
+
+
+
+
 
 
 
