faa200e90506fdd91fa24ddcf775a819262d9dde
Your Name <you@example.com>
2021-01-17 23:13:43 -0800

    create a formula

---
+++ djfnb.js
@@ -0,0 +1,46 @@
+/*
+let s = +prompt("whats is s?");
+let d = +prompt("whats is d?");
+let v = s / d;
+console.log (`${v}`);
+*/
+
+/*
+let s = +prompt ('what is S?');
+let r = +prompt ('what is r ?');
+let y = +prompt ('what is y ?');
+let h = s/100 * r/100 * y/100 ;
+console.log (`${h}`);
+*/
+
+
+/*
+let n = +prompt ('what is n?')
+let a = +prompt ('what is the a?')
+let q = +prompt ('what is the q ?')
+let pas = n*a/q; 
+
+console.log (`${pas}`)
+*/
+/*
+let n = +prompt ('what is n?')
+let a = +prompt ('what is the a?')
+let q = +prompt ('what is the q ?')
+let pas = n*a+q; 
+
+console.log (`${pas}`)
+
+*/
+
+let n = +prompt ('what is n?')
+let a = +prompt ('what is the a?')
+let q = +prompt ('what is the q ?')
+let h = +prompt ('what is n?')
+
+
+let pas = n*a+q/h; 
+
+console.log (`${pas}`)
+
+
+
--- first.js
+++ first.js
@@ -3,20 +3,33 @@ let secound = first;
 let third = secound ;
 let fourth = third;
 let fifth = fourth;
-
 let city = "city";
 let country = "country";
-
 let author = "oleg shestinskii"
 let  title = "blokada"
+let artistes = "artistes";
+let movie = "so"
+
+//hw
+/*let firstname = prompt('what is your name?');*/
+//hard p
+/*
+let a = +prompt("whats is a?");
+let b = +prompt("whats is b?");
+let sum = a + b;*/
+
+let s = +prompt("whats is s?");
+let d = +prompt("whats is d?");
+let sum = s * d;
 
  unit = "before"
  subjeckt = "today "
 unit = "temarature"
 subjeckt = "pinaple"
-
 author = "kendji malle"
 title = "le gitange"
+artistes = "kendji jirack";
+
 
    console.log(fifth)
 
@@ -26,10 +39,34 @@ title = "le gitange"
    
  console.log( "my favorite book is " + author +"by" + title + "!" )
 
+  console.log ("my favorite songs is  \"color Gitano\" by Kendji ")
+  console.log ("my favorite songs is  'color Gitano' by Kendji ")
+
+ console.log(  "she's the best" )
+
+ //console.log (" i'am  the fan of "  \" +  artistes + \" )
+
+ console.log ( ` my favorite songs ` + artistes + "!" )
+
+ console.log (` 'my favorite movie is \"terminator\" ' ` )
+
+ // pos v
+ console.log (` my favorite movie is "` + movie + `"`);
+
+ console.log (` my favorite movie is "${movie}"`);
 
 
+//hw
+/*console.log(`your name is ${firstname}`);*/
+
+/*
+//hard p
+console.log (`the sum of two numbers is equal to ${sum}`)
+*/
 
 
+//posl p
+console.log (`s multipliied by d is equal to ${sum}`);
 
 
 
