2406b295fc355d7682e8c7cce5d83d7f2b81ce42
Your Name <you@example.com>
2021-01-17 23:49:28 -0800

    added console.log

--- djfnb.js
+++ djfnb.js
@@ -1,31 +1,25 @@
 
-/*
+
 let s = +prompt("whats is s?");
 let d = +prompt("whats is d?");
 let v = s / d;
 
-*/
-/*
+
 let s = +prompt ('what is S?');
 let r = +prompt ('what is r ?');
 let y = +prompt ('what is y ?');
 let h = s/100 * r/100 * y/100 ;
-*/
 
-
-/*
 let n = +prompt ('what is n?')
 let a = +prompt ('what is the a?')
 let q = +prompt ('what is the q ?')
 let summ = n*a/q; 
-*/
-/*
+
 let n = +prompt ('what is n?')
 let a = +prompt ('what is the a?')
 let q = +prompt ('what is the q ?')
 let itog  = n*a+q; 
-console.log (`${itog}`)
-*/
+
 let n = +prompt ('what is n?')
 let a = +prompt ('what is the a?')
 let q = +prompt ('what is the q ?')
@@ -33,22 +27,10 @@ let h = +prompt ('what is n?')
 let pas = n*a+q/h; 
 
 
-
-
-
 console.log (`${v}`);
 console.log (`${h}`);
 console.log (`${summ}`)
-
-
-
-
-
-
-
-
-
-
+console.log (`${itog}`)
 console.log (`${pas}`)
 
 
